ea7e6e0ccad8b1bf7d74dfff3348ad542b77f10d
Álvaro Nogueira <98788990+alvaronogueira@users.noreply.github.com>
2023-07-06 16:25:56 -0300

    update http

---
+++ lib/services/journal_service.dart
@@ -0,0 +1,10 @@
+class JournalService {
+  static const String url = " http://192.168.3.14:3000/";
+  static const String resource = "learnhttp/";
+
+  String getUrl() {
+    return "$url$resource";
+  }
+
+  
+}
