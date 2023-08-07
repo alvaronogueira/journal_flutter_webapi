1b836cef2068a7077aa0a511649719fdee820ad6
Álvaro Nogueira <98788990+alvaronogueira@users.noreply.github.com>
2023-08-07 16:47:30 -0300

    update dialog.dart

---
+++ lib/screens/commom/confirmation_dialog.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+Future<dynamic> showConfirmationDialog(
+  BuildContext context, {
+  String title = "Attention!",
+  String content = "Are You sure?",
+  String affirmativeOption = "Confirm",
+}) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.pop(context, false);
+              },
+              child: Text("Cancel")),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context, true);
+            },
+            child: Text(
+              affirmativeOption.toUpperCase(),
+              style: const TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
